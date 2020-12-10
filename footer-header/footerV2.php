0209e97d3663534d88f3244435775c5a91cbb27d
jujudu78 <73346638+jujudu78@users.noreply.github.com>
2020-12-10 11:57:44 +0100

    Create footerV2.php

---
+++ footer-header/footerV2.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="Footer-header.css">
+    <div class="footer">
+
+    <span class="menudubaspartiegauche">
+        <a href="https://twitter.com"> <img id="twitter" src="twitter.png"></a>
+        <a href="https://www.instagram.com"> <img id="instagram" src="instagram.png"></a>
+        <a href="https://www.facebook.com"><img id="facebook" src="facebook.png"></a>
+        <a href="https://www.linkedin.com"><img id="linkedin" src="linkedin.png"></a>
+    </span>
+
+        <form class="menudubaspartiecentre">
+        <span class="Nous_contacter_titre">
+        Nous contacter
+        </span>
+            <div class="Nous_contacter_collect" data-validate="Merci de renseigner votre nom et prénom">
+                <input class="Nous_contacter_imput" type="text" name="name" placeholder="Nom et Prénom">
+                <span class="focus-Nous_contacter"></span>
+                <span></span></div>
+            <div class="Nous_contacter_collect" data-validate="Merci de renseigner votre adresse mail valide: e@a.x">
+                <input class="Nous_contacter_imput" type="text" name="email" placeholder="E-mail" >
+                <span class="focus-Nous_contacter"></span>
+                <span></span></div>
+            <div class="Nous_contacter_collect_imput_textarea" data-validate="Merci de renseigner votre message.">
+                <textarea class="Nous_contacter_imput_textarea" name="message" placeholder="Votre message"></textarea>
+                <span class="focus-Nous_contacter"></span>
+            </div>
+            <input type="submit" value="Envoyer"
+                   class="Nous_contacter_bouton" style="color: #484848"/>
+        </form>
+
+        <div class="adresse_premierepartie">
+            <a href="">10 rue de vanves, 92130</a>
+        </div>
+        <div class="adresse_deuxiemepartie">
+            <a href="">Issy-Les-Moulineaux, France</a>
+        </div>
+        <div class="horaire">
+            <a href="">8h30-12h, 13h-18h</a>
+        </div>
+        <div class="telephone">
+            <a href="">07 81 06 65 85</a>
+        </div>
+        <img id="maison" src="maison.png">
+        <img id="horloge" src="horloge.png">
+        <img id="telephone" src="telephone.png">
+    </div>
+
+
