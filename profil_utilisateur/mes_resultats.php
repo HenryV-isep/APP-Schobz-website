ef8f640c23aa28657fe7aeace8dcc1ce4d0ec8da
Henry VIEN <72934249+auchanVN@users.noreply.github.com>
2021-01-21 14:20:46 +0000

    Rename et fix affichage

--- profil_utilisateur/mes_resultats.php
+++ profil_utilisateur/mes_resultats.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="../footer_header/styleV3.css">
     <link rel="stylesheet" href="../profil_utilisateur/apercu_du_compte.css">
     <link rel="stylesheet" href="../Profil_utilisateur/mes_informations.css">
-    <link rel="stylesheet" href="../profil_utilisateur/mes_resultats.css">
+    <link rel="stylesheet" href="../Profil_utilisateur/mes_resultats.css">
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -61,13 +61,8 @@
     <h2>LES RESULTATS</h2>
     <p> Vous pouvez consulter vos résultats.</p>
     <div class="containtresults">
-        <table id="resulthistory">
-            <tr>
-                <th>Nom du test</th>
-                <th>Date</th>
-                <th>Résultats</th>
-            </tr>
-            <?php
+    <?php
+                //tu fais sa nomalement sur un autre page php, faut arreté du mettre tout le code sur la même page
                 $dBHost = "localhost";
                 $dBUser = "root";
                 $dBPassword = "root";
@@ -75,22 +70,46 @@
                 $port = "3306";
                 $conn = mysqli_connect($dBHost, $dBUser, $dBPassword, $dBDatabase, $port);
                 if (!$conn) {
-                    die("Échec de la connexion:" . mysqli_connect_error());
+                die("Échec de la connexion:" . mysqli_connect_error());
                 }
                 function table() {
+                    global $conn;
+                    $affichage = '';
                     $sql = "SELECT periode, nom_du_test, mesure_de_donnee FROM personne JOIN test ON `test`.`cle_etrangere_personne`=`personne`.`id_Personne` JOIN resultats ON `resultats`.`cle_etrangere_Test` = `test`.`id_test` WHERE nom='Cassoulet' ORDER BY periode DESC"; #Normalement, ça fera une liste
                     $resultData = mysqli_query($conn,$sql);
-                    while($row = $resultData->fetch_assoc()) {
-                        echo '<tr>
-                            <td>'.$row['periode'].'</td>
-                            <td>'.$row['nom_du_test'].'</td>
-                            <td>'.$row['mesure_de_donnee'].'</td>
+                    while($row = mysqli_fetch_assoc($resultData)) {
+                        $affichage = '
+                            <tr>
+                                <td>'.$row['periode'].'</td>
+                                <td>'.$row['nom_du_test'].'</td>
+                                <td>'.$row['mesure_de_donnee'].'</td>
                             </tr>';
                     }
+                    return $affichage;
                 }
-                $conn->close();
+
+                $sql = "SELECT mesure_de_donnee FROM `resultats` JOIN test ON `resultats`.`cle_etrangere_Test` = `test`.`id_test` JOIN `personne` ON `test`.`cle_etrangere_personne`=`personne`.`id_Personne` WHERE nom='Cassoulet'";
+                $resultData = mysqli_query($conn,$sql);
+                $row = mysqli_fetch_assoc($resultData);
+                if (isset($row)) {
+                    $affichage = table();
+                    echo '
+                        <table id="resulthistory">
+                            <thead>
+                                <tr>
+                                    <th>Nom du test</th>
+                                    <th>Date</th>
+                                    <th>Résultats</th>
+                                </tr>
+                            <thead>
+                            <tbody>
+                                '.$affichage.'
+                            </tbody>
+                        </table>';
+                } else {
+                        echo 'Il semblerait que vous n\'avez pas passé de test ';
+                }
             ?>
-        </table>
     </div>
 </div>
 <!-- Partie historique des résultats-->
