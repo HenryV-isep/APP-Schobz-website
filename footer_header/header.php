035d2e461508f3b4a31eb5dc1cb618c9dfce24a2
Henry VIEN <72934249+auchanVN@users.noreply.github.com>
2021-01-29 00:06:16 +0000

    Update header.php

--- footer_header/header.php
+++ footer_header/header.php
@@ -5,6 +5,9 @@
         <a href="../inscription_connexion/page_inscription.php">Mon compte</a>
         <a href="../a_propos_de_nous/a_propos_de_nous.php   ">A propos de nous</a>
     </span>
+    <span>
+        <a class="middle"> DriverPsychotechniqueTest.fr</a>
+    </span>
     <span class="menuduhautpartiedroite"> <!-- span c'est pour aligner les liens -->
         <a class="langue" href="../page_en/accueil_en.php"><img class="langue" src="../image/uk_flag.png"></a>
         <a class="link_CGU" href="#popup_CGU">Mentions légales et CGU </a>
